7ba08d12de3e087e4d3a59e3c903110b44a889e9
fiotkt <leehallwood@gmail.com>
2019-05-02 14:50:58 +0100

    Create cols.sql

---
+++ cols.sql
@@ -0,0 +1,22 @@
+col username format a25
+col PROFILE format a35
+col LIMIT   format a35
+
+col member format a40
+col table_name format a35
+col file_name format a50
+col name format a50
+col owner format a35
+col DIRECTORY_NAME format a25
+col DIRECTORY_PATH format a100
+col comments format a100
+
+col value format a60
+col PARAMETER format a50
+
+col column_name format a30
+col partition_name format a35
+col comments format a50
+col index_name format a35
+
+col SIGNATURE format 999999999999999999999999999999999
